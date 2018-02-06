509408d26fff525e4f1230a063a7058b4440c281
Chengtianlun <xyzlun@163.com>
2018-02-06 15:38:23 +0800

    creat MySQL table

--- Django/HelloWorld/HelloWorld/testdb.py
+++ Django/HelloWorld/HelloWorld/testdb.py
@@ -1,7 +1,6 @@
 #coding=utf-8
 from django.http import HttpResponse
-from TestModel.models improt Test
-
+from TestModel.models import Test
 def testdb(request):
     test1 = Test(name='runoob')
     test1.save()
--- Django/HelloWorld/TestModel/admin.py
+++ Django/HelloWorld/TestModel/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-import models
-admin.site.register(models.Test)
